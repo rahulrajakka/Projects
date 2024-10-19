6751b45ff8f6870b5d55e4fe5012100291b9ee78
rahulrajakka <akkarahulraj07@gmail.com>
2024-10-19 15:59:51 -0400

    add Nlog feature in UI tests to create log for every step.

--- UITests/Pages/HomePageMethods.cs
+++ UITests/Pages/HomePageMethods.cs
@@ -3,54 +3,71 @@ using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
 using System;
+using System.Collections.Generic;
+using NLog; // Adding NLog for logging
 
 namespace UITests.Pages
 {
     public class HomePageMethods
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();  // NLog logger instance
         private IWebDriver _driver;
         private WebDriverWait _wait;
+        private HomePageLocators _locators;
 
-        // Constructor to initialize WebDriver and WebDriverWait
         public HomePageMethods(IWebDriver driver)
         {
             _driver = driver;
-            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(15));  // Wait for elements to load
+            _locators = new HomePageLocators();
         }
 
-        // Hover over the "Solutions" menu
-        public void HoverOverSolutionsMenu()
+        // Hover over "Solutions" and click on "Market Intelligence"
+        public void NavigateToMarketIntelligence()
         {
+            Logger.Info("Hovering over 'Solutions' menu...");
             Actions actions = new Actions(_driver);
-            IWebElement solutionsElement = _driver.FindElement(HomePageLocators.solutionsMenu);
-            actions.MoveToElement(solutionsElement).Perform();
+
+            var solutionsMenu = _wait.Until(ExpectedConditions.ElementIsVisible(_locators.SolutionsMenu));
+            actions.MoveToElement(solutionsMenu).Perform();
+
+            Logger.Info("Clicking on 'Market Intelligence' submenu...");
+            var marketIntelligenceOption = _wait.Until(ExpectedConditions.ElementToBeClickable(_locators.MarketIntelligenceOption));
+            marketIntelligenceOption.Click();
         }
 
-        // Click on "Market Intelligence"
-        public void ClickMarketIntelligence()
+        // Retrieve headings in the "Ways You Benefit" section
+        public List<string> GetBenefitHeadings()
         {
-            IWebElement marketIntelligenceElement = _wait.Until(ExpectedConditions.ElementToBeClickable(HomePageLocators.marketIntelligenceOption));
-            marketIntelligenceElement.Click();
+            Logger.Info("Getting headings from the 'Ways You Benefit' section...");
+            List<string> headingsText = new List<string>();
+
+            var headings = _driver.FindElements(_locators.BenefitHeadings);
+            foreach (var heading in headings)
+            {
+                headingsText.Add(heading.Text);
+            }
+
+            Logger.Debug($"Retrieved {headingsText.Count} headings.");
+            return headingsText;
         }
 
-        // Scroll to "Let's Get Started" button
-        public void ScrollToLetsGetStarted()
-        {
-            IWebElement letsGetStartedElement = _wait.Until(ExpectedConditions.ElementToBeClickable(HomePageLocators.letsGetStartedButton));
-            ((IJavaScriptExecutor)_driver).ExecuteScript("arguments[0].scrollIntoView(true);", letsGetStartedElement);
-        }
-
-        // Click "Let's Get Started"
+        // Scroll to and click on the "Let's Get Started" button
         public void ClickLetsGetStarted()
         {
-            IWebElement letsGetStartedElement = _wait.Until(ExpectedConditions.ElementToBeClickable(HomePageLocators.letsGetStartedButton));
-            letsGetStartedElement.Click();
+            Logger.Info("Scrolling to and clicking 'Let’s Get Started' button...");
+            var letsGetStartedButton = _wait.Until(ExpectedConditions.ElementToBeClickable(_locators.LetsGetStartedButton));
+            ((IJavaScriptExecutor)_driver).ExecuteScript("arguments[0].scrollIntoView(true);", letsGetStartedButton);
+            letsGetStartedButton.Click();
         }
 
-        // Validate if we are on the "Contact" page
-        public bool IsContactPageLoaded()
+        // Validate that the Contact Page is displayed by checking the page title
+        public bool IsContactPageDisplayed()
         {
-            return _driver.Url.Contains("contact");
+            Logger.Info("Checking if Contact Page is displayed...");
+            bool isDisplayed = _wait.Until(ExpectedConditions.ElementIsVisible(_locators.ContactPageTitle)).Displayed;
+            Logger.Info(isDisplayed ? "Contact Page is displayed." : "Contact Page is not displayed.");
+            return isDisplayed;
         }
     }
 }
--- UITests/Script/ContactPageTC.cs
+++ UITests/Script/ContactPageTC.cs
@@ -1,29 +1,57 @@
 using NUnit.Framework;
 using UITests.Base;
 using UITests.Pages;
+using NLog; // Adding NLog for logging
+using System;
+using System.Collections.Generic;
 
 namespace UITests.Tests
 {
     [TestFixture]
-    public class ContactPageTest : TestBase
+    public class ContactPageTC : TestBase
     {
+        // Create NLog logger
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger(); 
         private HomePageMethods _homePageMethods;
 
         [SetUp]
-        public void Init()
+        public void Setup()
         {
+            Logger.Info("Initializing HomePageMethods for test...");
             _homePageMethods = new HomePageMethods(_driver);
         }
 
         [Test]
-        public void VerifyNavigationToContactPage()
+        public void ValidateContactPage()
         {
-            _driver.Navigate().GoToUrl("https://www.agdata.com");
-            _homePageMethods.HoverOverSolutionsMenu();
-            _homePageMethods.ClickMarketIntelligence();
-            _homePageMethods.ScrollToLetsGetStarted();
-            _homePageMethods.ClickLetsGetStarted();
-            Assert.IsTrue(_homePageMethods.IsContactPageLoaded());
+            try
+            {
+                // Navigate to Market Intelligence
+                Logger.Info("Navigating to Market Intelligence page...");
+                _homePageMethods.NavigateToMarketIntelligence();
+
+                // Get and print the headings in the "Ways You Benefit" section
+                Logger.Info("Retrieving the headings from 'Ways You Benefit' section...");
+                List<string> benefitHeadings = _homePageMethods.GetBenefitHeadings();
+                Logger.Debug("Benefit Headings retrieved: {0}", string.Join(", ", benefitHeadings));
+
+                // Click on the "Let's Get Started" button
+                Logger.Info("Clicking on 'Let's Get Started' button...");
+                _homePageMethods.ClickLetsGetStarted();
+
+                // Validate that the Contact Page is displayed
+                Logger.Info("Verifying that the Contact Page is displayed...");
+                bool isContactPageDisplayed = _homePageMethods.IsContactPageDisplayed();
+                Assert.IsTrue(isContactPageDisplayed, "Contact Page is not displayed");
+
+                Logger.Info("Test Passed: Contact Page is successfully displayed.");
+            }
+            catch (Exception ex)
+            {
+                // Log the exception and mark the test as failed
+                Logger.Error(ex, "Test Failed with Exception");
+                throw;  // Rethrow to ensure the test fails
+            }
         }
     }
 }
